68591365e75b74d7cd5c5047c314fbb5e9317ee2
CodingToastBread <mickeymouset@naver.com>
2024-06-15 16:09:35 +0900

    thymeleaf configuration done

--- src/main/java/coding/toast/springweblegacy/user/controller/UserController.java
+++ src/main/java/coding/toast/springweblegacy/user/controller/UserController.java
@@ -4,13 +4,32 @@ import coding.toast.springweblegacy.user.model.request.UserInfoRequest;
 import coding.toast.springweblegacy.user.model.response.UserInfoResponse;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
-@RestController
+@Controller
 public class UserController {
 	@GetMapping("/users")
+	public String userListInfo(Model model) {
+		model.addAttribute("userList",
+			List.of(
+				new UserInfoResponse("aaa", "dailyCode1", 23),
+				new UserInfoResponse("bbb", "dailyCode2", 24),
+				new UserInfoResponse("ccc", "dailyCode3", 25),
+				new UserInfoResponse("ddd", "dailyCode4", 26),
+				new UserInfoResponse("eee", "dailyCode5", 27)
+			));
+		return "user/userList";
+	}
+	
+	@GetMapping("/users/{id}")
+	@ResponseBody
 	public UserInfoResponse userInfoResponse(@Valid UserInfoRequest userInfoRequest) {
 		String id = userInfoRequest.id();
 		UserInfoResponse infoResponse = UserInfoResponse.builder()
---
+++ src/main/java/coding/toast/springweblegacy/welcome/WelcomeController.java
@@ -0,0 +1,13 @@
+package coding.toast.springweblegacy.welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@GetMapping("/")
+	public String welcomePage() {
+		return "index";
+	}
+}
