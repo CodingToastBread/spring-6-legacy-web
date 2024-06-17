4e54b987d4b75a87f1ecbbfe3db4ad4e34e958be
sampark1234 <sampark1234@wavus.co.kr>
2024-06-17 12:02:56 +0900

    add some sample comments

--- src/main/java/coding/toast/springweblegacy/user/data/table/User.java
+++ src/main/java/coding/toast/springweblegacy/user/data/table/User.java
@@ -19,4 +19,12 @@ create table users
     phone_number varchar(50)
 );
 
+-- sample data
+insert into users (name, phone_number)
+values ('Daily Code', '010-0000-0000'),
+       ('Coding Toast', '010-1111-1111'),
+       ('Working Toast', '010-2222-2222'),
+       ('Sleeping Toast', '010-4444-4444');
+
+
  */
