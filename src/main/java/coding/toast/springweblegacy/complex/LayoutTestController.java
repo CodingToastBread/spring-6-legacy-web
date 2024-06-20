15c9c51c07f410d173bb13ef3225d09f4a844bd9
sampark1234 <sampark1234@wavus.co.kr>
2024-06-20 13:26:40 +0900

    Add some layout examples using [thymeleaf-layout-dialect]

---
+++ src/main/java/coding/toast/springweblegacy/complex/LayoutTestController.java
@@ -0,0 +1,19 @@
+package coding.toast.springweblegacy.complex;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/layout")
+@RequiredArgsConstructor
+public class LayoutTestController {
+
+    @GetMapping("/sample")
+    public String layout1(Model model) {
+        model.addAttribute("passValue", "test text");
+        return "layouts/sample-layout-content";
+    }
+}
